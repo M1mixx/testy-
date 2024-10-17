117f358ab68b2299711b3b420669efb1ac14b5c6
M1mixx <maxim2008.34.9@gmail.com>
2024-10-17 18:31:15 +0300

    lab

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,15 +1,15 @@
-﻿#include <SDL.h>
-#include <iostream>
+﻿#include <SDL.h> 
+#include <iostream> 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Инициализация SDL
+    // Инициализация SDL 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "Ошибка инициализации SDL: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    // Создаем окно
+    // Создаем окно 
     SDL_Window* window = SDL_CreateWindow("Простая игра на SDL2",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         800, 600, SDL_WINDOW_SHOWN);
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // Создаем рендер
+    // Создаем рендер 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         std::cout << "Ошибка создания рендера: " << SDL_GetError() << std::endl;
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // Загрузка изображения персонажа
+    // Загрузка изображения персонажа 
     SDL_Surface* tempSurface = SDL_LoadBMP("character.bmp");
     if (!tempSurface) {
         cout << "Ошибка загрузки изображения: " << SDL_GetError() << endl;
@@ -40,28 +40,28 @@ int main(int argc, char* argv[]) {
     SDL_Texture* characterTexture = SDL_CreateTextureFromSurface(renderer, tempSurface);
     SDL_FreeSurface(tempSurface);
 
-    SDL_Rect characterRect = { 100, 100, 64, 64 }; // Положение и размер персонажа
-    const int speed = 1; // Скорость перемещения
+    SDL_Rect characterRect = { 100, 100, 64, 64 }; // Положение и размер персонажа 
+    const int speed = 1; // Скорость перемещения 
 
     int windowWidth = 800;
     int windowHeight = 600;
 
-    // Основной цикл игры
+    // Основной цикл игры 
     bool isRunning = true;
     SDL_Event event;
 
     while (isRunning) {
-        // Обрабатываем события
+        // Обрабатываем события 
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 isRunning = false;
             }
         }
 
-        // Получение состояния клавиш
+        // Получение состояния клавиш 
         const Uint8* keystate = SDL_GetKeyboardState(NULL);
 
-        // Обработка нажатий клавиш WASD
+        // Обработка нажатий клавиш WASD 
         if (keystate[SDL_SCANCODE_W] && characterRect.y > 0) {
             characterRect.y -= speed;
         }
@@ -71,22 +71,22 @@ int main(int argc, char* argv[]) {
         if (keystate[SDL_SCANCODE_A] && characterRect.x > 0) {
             characterRect.x -= speed;
         }
-        if (keystate[SDL_SCANCODE_D] && characterRect.x + characterRect.w < windowHeight) {
+        if (keystate[SDL_SCANCODE_D] && characterRect.x + characterRect.w < windowWidth) {
             characterRect.x += speed;
         }
 
-        // Очищаем экран
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Черный фон
+        // Очищаем экран 
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Черный фон 
         SDL_RenderClear(renderer);
 
-        // Отрисовка персонажа
+        // Отрисовка персонажа 
         SDL_RenderCopy(renderer, characterTexture, NULL, &characterRect);
 
-        // Обновляем экран
+        // Обновляем экран 
         SDL_RenderPresent(renderer);
     }
 
-    // Очищаем ресурсы
+    // Очищаем ресурсы 
     SDL_DestroyTexture(characterTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
